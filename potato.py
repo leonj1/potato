87be992d463d2136d55a2329bb8408bc4534748a
Jose Leon <leonj1@gmail.com>
2018-01-08 06:28:30 -0500

    Support to pass credentials

--- potato.py
+++ potato.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+import json
 
 login_url = 'https://put.io/login'
 # next should be empty
@@ -10,10 +11,16 @@ login_data = {
 }
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", help="file to push")
+parser.add_argument("--user", help="put.io username")
+parser.add_argument("--password", help="put.io password")
+parser.add_argument("--file", help="file to upload")
 args = parser.parse_args()
+
+credentials  = login_data
+credentials ['username'] = args.user
+credentials ['password'] = args.password
 
 files = {'upload_file': open(args.file,'rb')}
 s = requests.Session()
-r = requests.post(login_url, files=files, data=login_data)
+r = requests.post(login_url, files=files, data=credentials)
 
